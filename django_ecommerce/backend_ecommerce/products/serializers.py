662d7a4ec6c9dae2c105c64ac9df98b58b2ba3dd
ponydmt0906 <125996497+ponydmt0906@users.noreply.github.com>
2024-06-07 16:21:17 +0700

    Initial commit

---
+++ django_ecommerce/backend_ecommerce/products/serializers.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from . import models
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductImage
+        fields = (
+            'id',
+            'product_id',
+            'image_url',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        )
+
+
+class ProductCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductComment
+        fields = (
+            'id',
+            'rating',
+            'comment',
+            'product_id',
+            'user_id',
+            'parent_id',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+    comments = ProductCommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Product
+        depth = 1
+        fields = (
+            'id',
+            'name',
+            'unit',
+            'price',
+            'discount',
+            'amount',
+            'is_public',
+            'thumbnail',
+            'images',
+            'comments',
+            'category_id',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Category
+        depth = 1
+        fields = (
+            'id',
+            'name',
+            'slug',
+            'thumbnail_url',
+            'products',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        )
